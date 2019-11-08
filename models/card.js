746d5f4ff7a2c5e09e63152a455497550120d81a
Dmitry Gavrilov <ds33vladimir@yandex.ru>
2019-11-08 18:11:48 +0300

    forth commit-1

--- models/card.js
+++ models/card.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
-require('mongoose-type-url');
+const validate = require('mongoose-validator');
+
+const urlValidate = validate({
+  validator: 'matches',
+  // eslint-disable-next-line no-useless-escape
+  arguments: '/^((http|https):\/\/)((((\d{1,3}\.){3}(\d{1,3})(:[0-9]{2,5}(\/[A-Za-z\/]+)?)?)$)|((w{3}\.)?[A-Za-z0-9\/]+[.]+(([A-Za-z0-9\/]+)|([A-Za-z]+(\:[0-9]{2,5})?))(#)?$))',
+});
 
 const cardSchema = new mongoose.Schema({
   name: {
@@ -9,8 +15,9 @@ const cardSchema = new mongoose.Schema({
     maxlength: 30,
   },
   link: {
-    type: mongoose.SchemaTypes.Url,
+    type: String,
     required: true,
+    validate: urlValidate,
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
