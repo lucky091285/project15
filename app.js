8bda9799f489b9305fd8470af463749131bcc43c
Dmitry Gavrilov <ds33vladimir@yandex.ru>
2019-11-09 21:37:03 +0300

    sixth commit

--- app.js
+++ app.js
@@ -57,7 +57,8 @@ app.use(errorLogger);
 
 app.use(errors()); // обработчик ошибок celebrate
 
-app.use((err, req, res) => {
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res
     .status(statusCode)
